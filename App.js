b4da025c87d2735f969975d87d5a7524f3f6a0ef
Dilan Madhusanka <dilanm@insharptechnologies.com>
2021-09-19 20:23:47 +0530

    create splash screen

--- App.js
+++ App.js
@@ -1,10 +1,7 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import SplashScreen from './screens/SplashScreen';
-import LoginScreen from './screens/LoginScreen';
 import MainTabScreen from './screens/MainTabScreen';
 
 const Stack = createNativeStackNavigator();
--- screens/SplashScreen.js
+++ screens/SplashScreen.js
@@ -1,14 +1,34 @@
 import React from "react";
-import { View, Text, Button, StyleSheet } from 'react-native';
+import { View, Image, Text, Button, StyleSheet, TouchableOpacity } from 'react-native';
+import { useFonts, Inter_900Black } from '@expo-google-fonts/inter';
+
+import splashImage from '../assets/pet_care_splash_screen.png';
+
+const title = 'Get Start';
 
 const SplashScreen = ({navigation}) => {
     return (
         <View style={styles.container}>
-          <Text>Splash Screen</Text>
-          <Button
-            title="Get Start"
-            onPress={()=> navigation.navigate("MainTabScreen")}
-          />
+
+          <View style={styles.header}>
+            <View>
+                <Image 
+                    source={splashImage}
+                    style={{width: 300 , height: 300 }}
+                />
+            </View>
+
+            <View>
+                <Text style={styles.headerTitleText}>Pet Care</Text>
+                <Text style={styles.headerBodyText}>Helping you to take good care of your pet</Text>
+            </View>
+          </View>
+
+          <TouchableOpacity onPress={()=> navigation.navigate("MainTabScreen")} style={styles.appButtonContainer}>
+                <Text style={styles.appButtonText}>{title}</Text>
+        </TouchableOpacity>
+
+          
         </View>
       );
 };
@@ -18,9 +38,42 @@ export default SplashScreen;
 const styles = StyleSheet.create({
     container: {
       flex: 1,
-      backgroundColor: '#fff',
-      alignItems: 'center',
-      justifyContent: 'center',
+      backgroundColor: '#FF7070',
+      padding: 10
     },
-  });
+    header: {
+        flex:1,
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    headerTitleText: {
+        color: '#fff',
+        fontSize: 35,
+        alignSelf: 'center',
+        marginTop: 50,
+    },
+    headerBodyText: {
+        fontSize: 17,
+        color: '#fff',
+        alignSelf: 'center',
+        marginTop: 5
+    },
+    appButtonContainer: {
+        elevation: 5,
+        backgroundColor: "#fff",
+        borderRadius: 10,
+        paddingVertical: 10,
+        paddingHorizontal: 12,
+        marginBottom: 20,
+        marginHorizontal: 50
+      },
+      appButtonText: {
+        fontSize: 18,
+        color: "#000",
+        fontWeight: "bold",
+        alignSelf: "center",
+        textTransform: "uppercase",
+        opacity: 0.7,
+      }
+});
   
