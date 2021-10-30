8f969487e305e7748f6b6fe254de4040d462e5d6
Dilan Madhusanka <dilanm@insharptechnologies.com>
2021-10-30 16:42:51 +0530

    Add Problem Screen UI is ok

--- navigation/ExploreStack.js
+++ navigation/ExploreStack.js
@@ -10,7 +10,17 @@ export default function AuthStack() {
   return (
     <Stack.Navigator>
       <Stack.Screen name='ExploreHome' component={ExploreScreen} options={{ headerShown: false }} />
-      <Stack.Screen name='AddProblemView' component={AddProblemScreen} options={{ headerShown: false }} />
+      <Stack.Screen 
+        name='AddProblemView' 
+        component={AddProblemScreen} 
+        options={{
+             title: 'Add a Problem', 
+             headerTintColor: 'white',
+             headerLeft: null, 
+             headerStyle: {
+                 backgroundColor: '#FF7070'
+                 }, 
+        }} />
     </Stack.Navigator>
   );
 }
--- screens/AddProblemScreen.js
+++ screens/AddProblemScreen.js
@@ -1,77 +1,142 @@
 import React, { useState } from "react";
-import { View, SafeAreaView, FlatList, Text, StatusBar, StyleSheet } from 'react-native';
+import { Image, View, SafeAreaView, ScrollView, Text, StatusBar, StyleSheet } from 'react-native';
 import CardView from "../components/CardView";
-import { Button, InputField, ErrorMessage } from '../components';
+import { InputField, Button } from '../components';
 
 
 export default function AddProblemScreen() {
   
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
-    const [passwordVisibility, setPasswordVisibility] = useState(true);
-    const [rightIcon, setRightIcon] = useState('eye');
-    const [loginError, setLoginError] = useState('');
+    const [problemTitle, setProblemTitle] = useState('');
+    const [problemDescription, setProblemDescription] = useState('');
+    const [selectedImage, setSelectedImage] = useState(null);
 
   return (
-    <View style={styles.container}>
+    <SafeAreaView style={styles.container}>
+
+        <ScrollView showsVerticalScrollIndicator ={false}>
+
+        <Image style={styles.discImage} source={selectedImage!= null ?{ uri: selectedImage.localUri} : {uri: null}}  />
+
+        <View style = {styles.uploadButtonStyle}>
+        <Button
+            onPress={null}
+            backgroundColor='#FF7070'
+            title='Upload'
+            tileColor='#fff'
+            titleSize={20}
+            containerStyle={{
+              marginBottom: 10,
+              borderRadius: 100
+            }}
+        />
+      </View>
         
-      <InputField
-        inputStyle={{
-          fontSize: 14,
-        }}
-        containerStyle={{
-          backgroundColor: '#fff',
-          marginBottom: 20,
-          borderRadius: 10 ,
-          marginTop: 50
-        }}
-        placeholder=''
-        autoCapitalize='none'
-        keyboardType='default'
-        textContentType='none'
-        autoFocus={true}
-        value={email}
-        onChangeText={text => setEmail(text)}
-      />
-      <InputField
-        inputStyle={{
-          fontSize: 14,
-          height: 100,
-          justifyContent: "flex-start",
-          textAlignVertical: 'top'
-        }}
-        containerStyle={{
-          backgroundColor: '#fff',
-          marginBottom: 20,
-          borderRadius: 10 ,
-        }}
-        placeholder=''
-        autoCapitalize='none'
-        autoCorrect={false}
-        secureTextEntry={passwordVisibility}
-        textContentType='password'
-        value={password}
-        onChangeText={text => setPassword(text)}
-        multiline={true}
-      />
-    </View>
+        <View style={styles.problemTitleStyle}>
+
+            <Text style={styles.problemTitleTextStyle}>Problem Title</Text>
+
+            <InputField
+                inputStyle={{
+                fontSize: 14,
+                }}
+                containerStyle={{
+                backgroundColor: '#fff',
+                marginBottom: 20,
+                borderRadius: 10 ,
+                marginTop: 0
+                }}
+                placeholder=''
+                autoCapitalize='none'
+                keyboardType='default'
+                textContentType='none'
+                autoFocus={true}
+                value={problemTitle}
+                onChangeText={text => setProblemTitle(text)}
+            />
+      </View>
+      <View style={styles.problemDiscStyle}>
+
+            <Text style={styles.problemDiscTextStyle}>Problem Description</Text>
+
+            <InputField
+                inputStyle={{
+                fontSize: 14,
+                height: 100,
+                justifyContent: "flex-start",
+                textAlignVertical: 'top'
+                }}
+                containerStyle={{
+                backgroundColor: '#fff',
+                marginBottom: 20,
+                borderRadius: 10 ,
+                }}
+                placeholder=''
+                autoCapitalize='none'
+                autoCorrect={false}
+                textContentType='none'
+                value={problemDescription}
+                onChangeText={text => setProblemDescription(text)}
+                multiline={true}
+            />
+      </View>
+
+      <View >
+        <Button
+            onPress={null}
+            backgroundColor='#FF7070'
+            title='Submit'
+            tileColor='#fff'
+            titleSize={20}
+            containerStyle={{
+              marginBottom: 10,
+            }}
+        />
+      </View>
+
+</ScrollView>
+      
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    marginTop: StatusBar.currentHeight || 0,
     marginHorizontal:10
   },
 
-  item: {
-    backgroundColor: '#f9c2ff',
-    padding: 20,
-    marginVertical: 8,
-    marginHorizontal: 16,
+  problemTitleStyle: {
+      marginTop: 10,
   },
-  title: {
-    fontSize: 32,
+
+  problemTitleTextStyle: {
+      marginBottom: 10,
+      color: 'black',
+      fontWeight: 'bold'
   },
+
+  problemDiscStyle: {},
+
+  problemDiscTextStyle: {
+      marginBottom: 10,
+      color: 'black',
+      fontWeight: 'bold'
+  },
+
+  discImage: {
+    width: '100%',
+    height: 200,
+    alignSelf: 'center',
+    borderRadius: 10,
+    borderWidth: 2,
+    borderColor: 'gainsboro',
+    backgroundColor: 'gainsboro',
+    marginTop: 25,
+  },
+
+  uploadButtonStyle: {
+      marginHorizontal: 50,
+      marginTop: 20,
+      width:120,
+      alignSelf: 'center'
+  }
 });
